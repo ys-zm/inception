4cc2abffa91d587f90f25396af6813f32b7b9423
yesimzaim <yesimzaim@gmail.com>
2024-07-09 15:07:14 +0200

    fix mariadb entrypoint.sh error with root@localhost

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -15,10 +15,13 @@ else
     mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
     # Create user if not exists
-    mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
     # Grant privileges to user
-    mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO \`${MYSQL_USER}\`@'%';"
+    mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+
+    # # Grant all privileges to root user
+    # mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;"
 
     # Flush privileges to apply changes
     mariadb -e "FLUSH PRIVILEGES;"
@@ -27,9 +30,8 @@ else
 
     #--------------mariadb restart--------------#
     # Shutdown mariadb to restart with new config
-    mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 fi
 
 # Restart mariadb with new config in the background to keep the container running
 exec mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
-
