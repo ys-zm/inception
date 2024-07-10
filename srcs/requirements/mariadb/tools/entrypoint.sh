76035e84f5a3d7754d3a76f8d89b2bd0ec471bbd
Yesim Zaim <yzaim@f1r5s21.codam.nl>
2024-07-10 16:38:23 +0200

    fixed mariadb config a bit

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-# Check if the database directory already exists
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-    echo "Database already exists, skipping preparation"
-else
-    echo "Preparing default user and database"
+#--------------mariadb start--------------#
+service mariadb start    # Start MariaDB
+sleep 5                  # Wait for MariaDB to start
 
-    #--------------mariadb start--------------#
-    service mariadb start    # Start MariaDB
-    sleep 5                  # Wait for MariaDB to start
+#--------------mariadb config--------------#
+# Create database if not exists
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-    #--------------mariadb config--------------#
-    # Create database if not exists
-    mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+# Create user if not exists
+mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-    # Create user if not exists
-    mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# Grant privileges to user
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 
-    # Grant privileges to user
-    mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+# # Grant all privileges to root user -> run it in mariadb bash
+# mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;"
 
-    # # Grant all privileges to root user
-    # mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;"
+# Flush privileges to apply changes
+mariadb -e "FLUSH PRIVILEGES;"
 
-    # Flush privileges to apply changes
-    mariadb -e "FLUSH PRIVILEGES;"
+echo "Prepared default user and database"
 
-    echo "Prepared default user and database"
-
-    #--------------mariadb restart--------------#
-    # Shutdown mariadb to restart with new config
-    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
-fi
+#--------------mariadb restart--------------#
+# Shutdown mariadb to restart with new config
+ mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 
 # Restart mariadb with new config in the background to keep the container running
 exec mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+
